49f4813fc18809803973046a3e8b809a20aaccb2
Alexey Pavlov <102660478+AlexeyJarlax@users.noreply.github.com>
2026-02-01 17:22:05 +0300

    Update Kotlin toolchain versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,11 @@ if (localPropertiesFile.exists()) {
 plugins {
     id("com.android.application") version "8.9.2" apply false
     id("com.android.library") version "8.9.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.devtools.ksp") version "2.2.21-1.0.24"
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-    kotlin("plugin.serialization") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
 }
 
 buildscript {
