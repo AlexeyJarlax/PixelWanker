8fed3a53b3aa328baf02d9bbc41547a11d01ef4c
a9633238321 <a9633238321@gmail.com>
2026-05-12 19:27:14 +0300

    hotFix006

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         resourceConfigurations += setOf("ru", "en", "es", "hi", "fr", "pt", "ja")
         minSdk = 24 //Android 7
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.24"
+        versionCode = 25
+        versionName = "0.25"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
